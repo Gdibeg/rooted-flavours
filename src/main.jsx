aaedfe4f145a211cc623226bfbe49804ea83aa49
Sathwik Rao <sathwikrao903@gmail.com>
2026-03-11 15:25:11 +0530

    Fix GitHub Pages asset path

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
+import { HashRouter } from "react-router-dom";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { CartProvider } from "./context/CartContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <CartProvider>
+    <HashRouter>
       <App />
-    </CartProvider>
+    </HashRouter>
   </React.StrictMode>
 );
